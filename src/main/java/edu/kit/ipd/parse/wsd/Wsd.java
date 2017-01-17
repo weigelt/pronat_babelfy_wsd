b7989cb2dc5c25b6991fa3996dae4156ad08c5bd
Tobias Hey <tobias.hey@kit.edu>
2017-01-17 08:51:42 +0100

    First Version of WSD using Babelfy

---
+++ src/main/java/edu/kit/ipd/parse/wsd/Wsd.java
@@ -0,0 +1,215 @@
+package edu.kit.ipd.parse.wsd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+import org.kohsuke.MetaInfServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.kit.ipd.parse.luna.agent.AbstractAgent;
+import edu.kit.ipd.parse.luna.data.MissingDataException;
+import edu.kit.ipd.parse.luna.graph.IGraph;
+import edu.kit.ipd.parse.luna.graph.INode;
+import edu.kit.ipd.parse.luna.graph.Pair;
+import edu.kit.ipd.parse.wsd.utils.GraphUtils;
+import it.uniroma1.lcl.babelfy.commons.BabelfyParameters;
+import it.uniroma1.lcl.babelfy.commons.BabelfyParameters.MCS;
+import it.uniroma1.lcl.babelfy.commons.BabelfyParameters.MatchingType;
+import it.uniroma1.lcl.babelfy.commons.BabelfyParameters.ScoredCandidates;
+import it.uniroma1.lcl.babelfy.commons.BabelfyParameters.SemanticAnnotationResource;
+import it.uniroma1.lcl.babelfy.commons.BabelfyParameters.SemanticAnnotationType;
+import it.uniroma1.lcl.babelfy.commons.BabelfyToken;
+import it.uniroma1.lcl.babelfy.commons.PosTag;
+import it.uniroma1.lcl.babelfy.commons.annotation.SemanticAnnotation;
+import it.uniroma1.lcl.babelfy.core.Babelfy;
+import it.uniroma1.lcl.babelnet.BabelNet;
+import it.uniroma1.lcl.babelnet.BabelSynset;
+import it.uniroma1.lcl.babelnet.BabelSynsetID;
+import it.uniroma1.lcl.babelnet.InvalidBabelSynsetIDException;
+import it.uniroma1.lcl.jlt.util.Language;
+
+/**
+ * @author Tobias Hey
+ *
+ */
+
+@MetaInfServices(AbstractAgent.class)
+public class Wsd extends AbstractAgent {
+
+	private Babelfy bfy;
+
+	private BabelNet bn;
+
+	private static final String ID = "wsd";
+
+	private static final Logger logger = LoggerFactory.getLogger(Wsd.class);
+
+	private HashMap<String, PosTag> posTags;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.kit.ipd.parse.luna.agent.LunaObserver#init()
+	 */
+	@Override
+	public void init() {
+		setId(ID);
+		createPOSTags();
+		bn = BabelNet.getInstance();
+		BabelfyParameters bp = new BabelfyParameters();
+		bp.setAnnotationResource(SemanticAnnotationResource.WN);
+		bp.setAnnotationType(SemanticAnnotationType.ALL);
+		bp.setDensestSubgraph(true);
+		bp.setExtendCandidatesWithAIDAmeans(true);
+		bp.setMatchingType(MatchingType.PARTIAL_MATCHING);
+		bp.setMCS(MCS.OFF);
+		bp.setScoredCandidates(ScoredCandidates.ALL);
+		bp.setThreshold(0.5d);
+		bfy = new Babelfy(bp);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.kit.ipd.parse.luna.agent.AbstractAgent#exec()
+	 */
+	@Override
+	public void exec() {
+		IGraph graph = getGraph();
+		List<INode> utterance = new ArrayList<INode>();
+		try {
+			utterance = GraphUtils.getNodesOfUtterance(graph);
+		} catch (MissingDataException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		List<BabelfyToken> bfTokens = prepareInput(utterance);
+
+		List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(bfTokens, Language.EN);
+		int[] currentToken = new int[] { -1, -1 };
+		List<INode> nodes = null;
+		List<Pair<String, Double>> senses = null;
+		for (SemanticAnnotation semanticAnnotation : bfyAnnotations) {
+			if (!(semanticAnnotation.getTokenOffsetFragment().getStart() == currentToken[0]
+					&& semanticAnnotation.getTokenOffsetFragment().getEnd() == currentToken[1])) {
+				if (senses != null && nodes != null) {
+					writeToNodes(nodes, senses, graph);
+				}
+				nodes = getNodesForAnnotation(semanticAnnotation, utterance);
+				senses = new ArrayList<>();
+			}
+			try {
+				BabelSynset synset = bn.getSynset(new BabelSynsetID(semanticAnnotation.getBabelSynsetID()));
+				senses.add(new Pair<String, Double>(synset.getWordNetOffsets().get(0).getID(), semanticAnnotation.getScore()));
+				currentToken[0] = semanticAnnotation.getTokenOffsetFragment().getStart();
+				currentToken[1] = semanticAnnotation.getTokenOffsetFragment().getEnd();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidBabelSynsetIDException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		if (senses != null && nodes != null) {
+			writeToNodes(nodes, senses, graph);
+		}
+	}
+
+	private void writeToNodes(List<INode> nodes, List<Pair<String, Double>> senses, IGraph graph) {
+		Collections.sort(senses, new Comparator<Pair<String, Double>>() {
+
+			@Override
+			public int compare(Pair<String, Double> o1, Pair<String, Double> o2) {
+
+				return Double.compare(o1.getRight(), o2.getRight());
+			}
+		});
+		Collections.reverse(senses);
+		if (graph.hasNodeType("token")) {
+			if (!graph.getNodeType("token").getAllTypeAttributesTypesAndNames()
+					.contains(new Pair<String, String>("List<Pair<String, Double>>", "wsdSenses"))) {
+				graph.getNodeType("token").addAttributeToType("List<Pair<String, Double>>", "wsdSenses");
+			}
+			for (INode node : nodes) {
+				node.setAttributeValue("wsdSenses", senses);
+			}
+
+		} else {
+			logger.error("Graph has no Token Nodes!");
+		}
+
+	}
+
+	private List<BabelfyToken> prepareInput(List<INode> utterance) {
+		List<BabelfyToken> bfTokens = new ArrayList<>();
+		for (INode iNode : utterance) {
+			if (iNode.getType().getName().equals("token")) {
+				BabelfyToken token = new BabelfyToken((String) iNode.getAttributeValue("value"));
+				if (iNode.getAttributeNames().contains("pos") && !iNode.getAttributeValue("pos").equals("")) {
+					if (posTags.containsKey(iNode.getAttributeValue("pos"))) {
+						token.setPostag(posTags.get((String) iNode.getAttributeValue("pos")));
+					} else {
+						token.setPostag(PosTag.OTHER);
+						token.setLanguage(Language.EN);
+					}
+
+				}
+				bfTokens.add(token);
+			}
+		}
+		return bfTokens;
+	}
+
+	private List<INode> getNodesForAnnotation(SemanticAnnotation semanticAnnotation, List<INode> utterance) {
+		List<INode> result = new ArrayList<>();
+		int start = semanticAnnotation.getTokenOffsetFragment().getStart();
+		result.add(utterance.get(start));
+		while (start < semanticAnnotation.getTokenOffsetFragment().getEnd()) {
+			start++;
+			result.add(utterance.get(start));
+		}
+		return result;
+	}
+
+	/**
+	 * Creates a mapping between WordNet POS Tags (simple) and the POS Tags used
+	 * in this tagger (Penn Treebank tagset).
+	 * 
+	 * @return a HashMap mapping composite Tags (e.g. JJ, NNP, VB, etc.) with
+	 *         simple Tags (e.g. Adjective, Noun, Verb, etc.)
+	 */
+	private HashMap<String, PosTag> createPOSTags() {
+		posTags = new HashMap<String, PosTag>();
+
+		// Available types: POS.ADJECTIVE, POS.ADVERB, POS.NOUN, POS.VERB
+
+		//Keys: POS Tag, Values: POS
+
+		posTags.put("JJ", PosTag.ADJECTIVE);
+		posTags.put("JJR", PosTag.ADJECTIVE);
+		posTags.put("JJS", PosTag.ADJECTIVE);
+		posTags.put("NN", PosTag.NOUN);
+		posTags.put("NNP", PosTag.NOUN);
+		posTags.put("NNPS", PosTag.NOUN);
+		posTags.put("NNS", PosTag.NOUN);
+		posTags.put("RB", PosTag.ADVERB);
+		posTags.put("RBR", PosTag.ADVERB);
+		posTags.put("RBS", PosTag.ADVERB);
+		posTags.put("VB", PosTag.VERB);
+		posTags.put("VBD", PosTag.VERB);
+		posTags.put("VBG", PosTag.VERB);
+		posTags.put("VBN", PosTag.VERB);
+		posTags.put("VBP", PosTag.VERB);
+		posTags.put("VBZ", PosTag.VERB);
+
+		return posTags;
+	}
+
+}
---
+++ src/test/java/edu/kit/ipd/parse/wsd/WsdTest.java
@@ -0,0 +1,44 @@
+package edu.kit.ipd.parse.wsd;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.kit.ipd.parse.graphBuilder.GraphBuilder;
+import edu.kit.ipd.parse.luna.data.MissingDataException;
+import edu.kit.ipd.parse.luna.data.PrePipelineData;
+import edu.kit.ipd.parse.luna.pipeline.PipelineStageException;
+import edu.kit.ipd.parse.luna.tools.StringToHypothesis;
+import edu.kit.ipd.parse.shallownlp.ShallowNLP;
+
+public class WsdTest {
+
+	private static Wsd wsd;
+	private static GraphBuilder gb;
+	private static ShallowNLP snlp;
+
+	PrePipelineData ppd;
+
+	@BeforeClass
+	public static void setUp() {
+		wsd = new Wsd();
+		wsd.init();
+		gb = new GraphBuilder();
+		gb.init();
+		snlp = new ShallowNLP();
+		snlp.init();
+
+	}
+
+	@Test
+	public void StringTest() throws PipelineStageException, MissingDataException {
+		ppd = new PrePipelineData();
+		String input = "Armar please prepare me a meal there are plates in the dishwasher please get a plate from the dishwasher and clean it afterwards put the instant meal from the fridge on it and warm it in the microwave for two minutes when it is done get the plate out of the microwave and put it on the table then you are done";
+		ppd.setMainHypothesis(StringToHypothesis.stringToMainHypothesis(input));
+		snlp.exec(ppd);
+		gb.exec(ppd);
+		wsd.setGraph(ppd.getGraph());
+		wsd.exec();
+
+	}
+
+}
