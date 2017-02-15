7e6c3c38115300cd80c71ad69d99c92dc200f0f3
Tobias Hey <tobias.hey@kit.edu>
2017-02-15 17:29:51 +0100

    Revert "adapted to new graph structure "
    This reverts commit 58fe2650f7479c83f8ea23b2363fa1b8b62ac619.

--- src/main/java/edu/kit/ipd/parse/wsd/utils/GraphUtils.java
+++ src/main/java/edu/kit/ipd/parse/wsd/utils/GraphUtils.java
@@ -1,11 +1,13 @@
 package edu.kit.ipd.parse.wsd.utils;
 /**
- *
+ * 
  */
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,7 +59,7 @@ public final class GraphUtils {
 			if (graph instanceof ParseGraph) {
 				ParseGraph pGraph = (ParseGraph) graph;
 				INode current = pGraph.getFirstUtteranceNode();
-				List<? extends IArc> outgoingNextArcs = current.getOutgoingArcsOfType(nextArcType);
+				Set<? extends IArc> outgoingNextArcs = current.getOutgoingArcsOfType(nextArcType);
 				boolean hasNext = !outgoingNextArcs.isEmpty();
 				result.add(current);
 				while (hasNext) {
@@ -85,7 +87,7 @@ public final class GraphUtils {
 
 	public static final INode getNextNode(INode current, IGraph graph) {
 		IArcType arcType = graph.getArcType(NEXT_ARCTYPE_NAME);
-		List<? extends IArc> outgoingNextArcs = current.getOutgoingArcsOfType(arcType);
+		Set<? extends IArc> outgoingNextArcs = current.getOutgoingArcsOfType(arcType);
 		if (!outgoingNextArcs.isEmpty()) {
 			if (outgoingNextArcs.size() == 1) {
 				return outgoingNextArcs.toArray(new IArc[outgoingNextArcs.size()])[0].getTargetNode();
@@ -100,7 +102,7 @@ public final class GraphUtils {
 
 	public static final INode getPreviousNode(INode current, IGraph graph) {
 		IArcType arcType = graph.getArcType(NEXT_ARCTYPE_NAME);
-		List<? extends IArc> incomingNextArcs = current.getIncomingArcsOfType(arcType);
+		Set<? extends IArc> incomingNextArcs = current.getIncomingArcsOfType(arcType);
 		if (!incomingNextArcs.isEmpty()) {
 			if (incomingNextArcs.size() == 1) {
 				return incomingNextArcs.toArray(new IArc[incomingNextArcs.size()])[0].getSourceNode();
@@ -127,7 +129,7 @@ public final class GraphUtils {
 		INode current = arc.getTargetNode();
 		result.add(current);
 		if (hasOutgoingArcOfType(current, arc.getType().getName(), graph)) {
-			List<? extends IArc> arcs = current.getOutgoingArcsOfType(arc.getType());
+			Set<? extends IArc> arcs = current.getOutgoingArcsOfType(arc.getType());
 			for (IArc outArc : arcs) {
 				result.addAll(getNodesOfArcChain(outArc, graph));
 			}
@@ -141,7 +143,7 @@ public final class GraphUtils {
 		INode current = arc.getTargetNode();
 
 		if (hasOutgoingArcOfType(current, arc.getType().getName(), graph)) {
-			List<? extends IArc> arcs = current.getOutgoingArcsOfType(arc.getType());
+			Set<? extends IArc> arcs = current.getOutgoingArcsOfType(arc.getType());
 			for (IArc outArc : arcs) {
 				result.addAll(getArcsOfArcChain(outArc, graph));
 			}
@@ -158,7 +160,7 @@ public final class GraphUtils {
 	}
 
 	public static final void deleteNodeAndReferences(INode node, IGraph graph) {
-		List<? extends IArc> refs = node.getOutgoingArcsOfType(graph.getArcType("reference"));
+		Set<? extends IArc> refs = node.getOutgoingArcsOfType(graph.getArcType("reference"));
 		if (!refs.isEmpty()) {
 			for (IArc iArc : refs) {
 				List<IArc> refarcs = getArcsOfArcChain(iArc, graph);
